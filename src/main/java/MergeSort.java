8a50111bbd3e4dda92a31cc7511deb2a584e98fe
xmc1993 <1170143845@qq.com>
2016-09-28 15:24:00 +0800

    自底向上的归并排序

--- src/main/java/MergeSort.java
+++ src/main/java/MergeSort.java
@@ -15,6 +15,7 @@ public class MergeSort extends Sorts {
     /**
      * merge算法的基本要素要进行merge的两个数组必须是有序的
      * 最小的有序数组为大小为1的数组
+     *
      * @param arr
      * @param low
      * @param high
@@ -24,7 +25,23 @@ public class MergeSort extends Sorts {
         int mid = (low + high) / 2;
         sort(arr, low, mid);
         sort(arr, mid + 1, high);
-        mergeBeta(arr, low, mid, high);
+        merge(arr, low, mid, high);
+    }
+
+
+    /**
+     * 自底向上的归并排序
+     *
+     * @param arr
+     */
+    public static void sortFromBottom(Comparable[] arr) {
+        int N = arr.length;
+        aux = new Comparable[N];
+        for (int sz = 1; sz < N; sz *= 2) {
+            for (int lo = 0; lo < N - sz; lo += sz + sz) {
+                merge(arr, lo, lo + sz - 1, Math.min(lo + sz + sz - 1, N - 1));
+            }
+        }
     }
 
     public static void merge(Comparable[] arr, int low, int mid, int high) {
@@ -59,10 +76,10 @@ public class MergeSort extends Sorts {
             show(arr);
             System.out.println("------");
 
-            while ( (p <= mid) && less(aux[p], aux[q > high ? q-1 : q])) {
+            while ((p <= mid) && less(aux[p], aux[q > high ? q - 1 : q])) {
                 arr[t++] = aux[p++];
             }
-            while ((q <= high) && (!less(aux[p > mid ? p-1 : p], aux[q]))) {
+            while ((q <= high) && (!less(aux[p > mid ? p - 1 : p], aux[q]))) {
                 arr[t++] = aux[q++];
             }
         }
@@ -71,7 +88,8 @@ public class MergeSort extends Sorts {
 
     public static void main(String[] args) {
         String[] a = StdIn.readAllStrings();
-        sort(a);
+//        sort(a);
+        sortFromBottom(a);
         show(a);
     }
 
