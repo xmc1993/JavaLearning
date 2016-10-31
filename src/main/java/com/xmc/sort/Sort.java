a02b69a3e229052e5858b63776e327ff2be52905
xmc1993 <1170143845@qq.com>
2016-10-31 17:33:37 +0800

    quick

---
+++ src/main/java/com/xmc/sort/Sort.java
@@ -0,0 +1,27 @@
+package com.xmc.sort;
+
+/**
+ * Created by xmc1993 on 16/10/31.
+ */
+public class Sort {
+
+    public static void quickSort(int[] arr){
+
+    }
+
+    public static void quickSort(int[] arr, int low, int high){
+
+    }
+
+    public static void partation(int[] arr, int low, int high){
+        int key = arr[low];
+        int m = low;
+        int n = high + 1;
+        while(true){
+            while(arr[++m] > key) if (m == n) return;
+            while(arr[--n] < key)if(m == n) return;
+            if(m >= n) return;
+        }
+//        if()
+    }
+}
