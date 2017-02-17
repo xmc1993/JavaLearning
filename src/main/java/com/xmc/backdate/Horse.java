6bf6eccaedae2c854ea501d5cbc3867aecbe6ce6
xmc1993 <1170143845@qq.com>
2017-02-17 17:32:54 +0800

    todo

--- src/main/java/com/xmc/backdate/Horse.java
+++ src/main/java/com/xmc/backdate/Horse.java
@@ -4,5 +4,21 @@ package com.xmc.backdate;
  * Created by xmc1993 on 17/2/17.
  */
 public class Horse {
+    public static int[] xs = {2, 1, -2, -1, -2, -1, 2, 1};
+    public static int[] ys = {1, 2, 1, 2, -1, -2, -1, -2};
+    public static int count = 0;
+    public static int[][] square = new int[4][5];
+    public static int X = 2;  //4596
+    public static int Y = 2;
 
+    public static void main(String[] args) {
+
+    }
+
+    public static void search(int x, int y) {
+
+        for (int i = 0; i < 8; i++) {
+
+        }
+    }
 }
